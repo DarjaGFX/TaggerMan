88fc11335f2008e95098af770e58017b64533c13
ali <ali.jafari20@gmail.com>
2020-04-26 06:52:48 +0430

    items can be selected now

--- explorer.py
+++ explorer.py
@@ -53,12 +53,21 @@ def draw(letter, selectedIndex=0):
     curses.start_color()
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    for i in range(len(letter)):
+        if letter[i]['selected']:
+            screen.addstr(
+                            letter[i]['posx'],
+                            letter[i]['posy'],
+                            "#",
+                            curses.color_pair(2)
+                         )
     screen.addstr(
                     letter[selectedIndex]['posx'],
                     letter[selectedIndex]['posy'],
                     "#",
                     curses.color_pair(1)
-                 )  # , curses.color_pair(197))
+                 )
     screen.refresh()
 
 
@@ -70,24 +79,24 @@ def exlpore(pwd=None):
     if pwd is None:
         pwd = CWD
     objs = {
-            0: {'text': 'Insert tag', 'posx': 1, 'posy': 1, 'type': 'command'},
-            1: {'text': 'Remove tag', 'posx': 1, 'posy': 17, 'type': 'command'},
-            2: {'text': 'Modify tag', 'posx': 1, 'posy': 33, 'type': 'command'},
-            3: {'text': 'Search by tag', 'posx': 1, 'posy': 49, 'type': 'command'},
-            4: {'text': 'Exit', 'posx': 1, 'posy': 73, 'type': 'command'},
-            5: {'text': pwd, 'posx': 3, 'posy': 1, 'type': 'directory'},
-            6: {'text': '..', 'posx': 5, 'posy': 1, 'type': 'directory'},
+            0: {'text': 'Insert tag', 'posx': 1, 'posy': 1, 'type': 'command', 'selected': False},
+            1: {'text': 'Remove tag', 'posx': 1, 'posy': 17, 'type': 'command', 'selected': False},
+            2: {'text': 'Modify tag', 'posx': 1, 'posy': 33, 'type': 'command', 'selected': False},
+            3: {'text': 'Search by tag', 'posx': 1, 'posy': 49, 'type': 'command', 'selected': False},
+            4: {'text': 'Exit', 'posx': 1, 'posy': 73, 'type': 'command', 'selected': False},
+            5: {'text': pwd, 'posx': 3, 'posy': 1, 'type': 'directory', 'selected': False},
+            6: {'text': '..', 'posx': 5, 'posy': 1, 'type': 'directory', 'selected': False},
            }
     indx = 6
     for d in childir(pwd):
         if DONT_SHOW_HIDDEN and d[0] == '.':
             continue
-        objs.update({len(objs): {'text': d, 'posx': indx, 'posy': 1, 'type': 'directory'}})
+        objs.update({len(objs): {'text': d, 'posx': indx, 'posy': 1, 'type': 'directory', 'selected': False}})
         indx += 1
     for f in childfiles(pwd):
         if DONT_SHOW_HIDDEN and f[0] == '.':
             continue
-        objs.update({len(objs): {'text': f, 'posx': indx, 'posy': 1, 'type': 'file'}})
+        objs.update({len(objs): {'text': f, 'posx': indx, 'posy': 1, 'type': 'file', 'selected': False}})
         indx += 1
     draw(objs, selectedIndex)
     try:
@@ -109,11 +118,13 @@ def exlpore(pwd=None):
                 else:
                     selectedIndex -= 1
                     draw(objs, selectedIndex)
-            elif char == 10:
+            elif char == 10:  # Enter
                 if objs[selectedIndex]['type'] == 'directory':
                     if objs[selectedIndex]['text'] == '..':
                         return exlpore(pwd=parentdir(objs[5]['text']))
                     return exlpore(pwd=os.path.join(pwd, objs[selectedIndex]['text']))
+            elif char == 32:  # Space
+                objs[selectedIndex]['selected'] = not objs[selectedIndex]['selected']
     except PermissionError:
         exlpore(pwd=pwd)
     except:
