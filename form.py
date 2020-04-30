6fba128bb604025374ff807dd5918e2240f50745
ali <ali.jafari20@gmail.com>
2020-04-30 13:45:42 +0430

    input form added

--- form.py
+++ form.py
@@ -105,10 +105,12 @@ class BOOLEAN_FORM():
                     self.startIndex -= 1
                 else:
                     self.startIndex = 0
-                self.draw(messageTitle, messageText, self.selected, self.startIndex)
+                self.draw(
+                    messageTitle, messageText, self.selected, self.startIndex)
             if ch == curses.KEY_DOWN:
                 self.startIndex += 1
-                self.draw(messageTitle, messageText, self.selected, self.startIndex)
+                self.draw(
+                    messageTitle, messageText, self.selected, self.startIndex)
             if ch == ENTER:
                 curses.nocbreak()
                 self.screen.keypad(0)
@@ -214,3 +216,96 @@ class ONE_BUTTON_FORM():
                 curses.echo()
                 curses.endwin()
                 return
+
+
+class INPUT_FORM():
+    def __init__(self):
+        self.screen = curses.initscr()
+        # curses.curs_set(False)
+        # curses.noecho()
+        curses.cbreak()
+        self.screen.keypad(True)
+        self.selected = False
+        self.True_text = 'OK'
+        self.False_text = 'CANCEL'
+        self.input = ''
+
+    def draw(self, messageTitle, selected):
+        self.screen.clear()
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
+        ymax, xmax = self.screen.getmaxyx()
+        x_start = int(xmax/4)
+        y_start = int(ymax/4)
+
+        for i in range(x_start, x_start*3+1):
+            self.screen.move(y_start-4, i)
+            self.screen.addstr('=')
+            self.screen.move(y_start, i)
+            self.screen.addstr('=')
+            if i >= x_start+2 and i <= x_start*3-1:
+                self.screen.move(y_start+2, i)
+                self.screen.addstr('-')
+            self.screen.move(y_start+4, i)
+            self.screen.addstr('=')
+            self.screen.move(y_start+6, i)
+            self.screen.addstr('=')
+        for i in range(y_start-3, y_start+6):
+            if i == y_start:
+                continue
+            self.screen.move(i, x_start)
+            self.screen.addstr('|')
+            self.screen.move(i, x_start*3)
+            self.screen.addstr('|')
+        true_x = x_start+int(x_start/2)
+        false_x = 2*x_start+int(x_start/3)
+        self.screen.move(y_start+5, true_x)
+        if selected:
+            self.screen.addstr('['+self.True_text+']', curses.color_pair(1))
+        else:
+            self.screen.addstr('['+self.True_text+']')
+        self.screen.move(y_start+5, false_x)
+        if selected:
+            self.screen.addstr('['+self.False_text+']')
+        else:
+            self.screen.addstr('['+self.False_text+']', curses.color_pair(2))
+
+        # Print Title
+        Title_x = (2*x_start) - int(len(messageTitle)/2)
+        Title_y = y_start - 2
+        self.screen.move(Title_y, Title_x)
+        self.screen.addstr(messageTitle)
+
+        # Print Message
+        inputShow = ' '+self.input+' '
+        x = (2*x_start) - int(len(inputShow)/2)
+        y = y_start+2
+        self.screen.move(y, x)
+        self.screen.addstr(inputShow)
+
+    def show(self, messageTitle=''):
+        self.draw(messageTitle, self.selected)
+        while True:
+            ch = self.screen.getch()
+            if ch == curses.KEY_RIGHT or ch == curses.KEY_LEFT:
+                self.selected = not self.selected
+                self.draw(messageTitle, self.selected)
+            elif ch == ENTER:
+                if self.selected:    
+                    curses.nocbreak()
+                    self.screen.keypad(0)
+                    curses.echo()
+                    curses.endwin()
+                    if self.input != '':
+                        return self.input
+                    return None
+                else:
+                    return None
+            elif ch == curses.KEY_BACKSPACE:
+                self.input = self.input[0:-2]
+                self.draw(messageTitle, self.selected)
+            else:
+                self.input += chr(ch)
+                self.draw(messageTitle, self.selected)
